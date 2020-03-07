3ea33fa834d4e14f69176072b1f18a5824fb1319
Dan Rusu <dan.rusu@rms.com>
2020-03-07 08:08:34 +0200

    update for second course

--- template/template.js
+++ template/template.js
@@ -1,15 +1,16 @@
 // document.querySelectorAll uses a CSS elector for finding elements in the DOM
-const headersNodelist = document.querySelectorAll('h1'); 
+const headersNodelist = document.querySelectorAll('h1');  // get all h1 elements
 
 const headers = [].slice.apply(headersNodelist); // convert from NodeList to Array
+//const headers = [].slice.call(headersNodelist);
 
-const header = headers[0];
+const header = headers[0]; // first header 
 var headerClickCount = 0
 
 console.log(`Header: ${header.innerHTML}`);
 
 function messageAlert(){
-	console.log(`clicked ${headerClickCount++} on ${header}`);
+	console.log(`click ${headerClickCount++} on ${header}`);
 	alert('This is a "click" event handler on h1 element');
 }
 
@@ -21,5 +22,5 @@ const messageAlert = () => {
 };
 */
 
-// add a function as handler for an HTML event
+// add messageAlert function as handler for the 'click' HTML event
 header.addEventListener('click', messageAlert);
